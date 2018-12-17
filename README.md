a30c11ce54a2a5419ae1d7dfe87204983c32d505
hamzakhan7861 <45925364+hamzakhan7861@users.noreply.github.com>
2018-12-18 10:10:48 +1100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# MyDna-TrainingModule
-Created with CodeSandbox
---
+++ src/components/Tips/MoreTips.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+import IdentifyingCustomers from "./IdentifyingCustomers";
+
+const styles = theme => ({
+  root: {
+    width: "100%"
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular
+  },
+  panel: {
+    backgroundColor: "#aee7f9"
+  }
+});
+
+function MoreTips(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <ExpansionPanel className={classes.panel}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>
+            Engaging More Doctors
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <div>content goes here...</div>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel className={classes.panel}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>
+            Identifying Customer
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <div>
+            <Typography>
+              Identify patient and discuss the test with them. A potential
+              customer can be one of the following:
+            </Typography>
+            <br />
+            <IdentifyingCustomers />
+          </div>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel className={classes.panel}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>
+            In-depth Materials
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <div>
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography className={classes.heading}>
+                Genetic Information
+              </Typography>
+            </ExpansionPanelSummary>
+          </div>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+  );
+}
+
+export default withStyles(styles)(MoreTips);
